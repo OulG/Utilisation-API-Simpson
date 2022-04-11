c19363043a70b0a47808e17e5ff3e41f4947931d
OulG <lou.gain@outlook.fr>
2022-04-11 10:43:22 +0200

    Utilisation d'une API pour appeler des citations des Simpson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
+import DisplayQuote from './Components/DisplayQuote.jsx';
 
 function App() {
+  
+  const sampleQuote = {
+      quote:
+        "These are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will.",
+      character: 'Lisa Simpson',
+      image:
+        'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083',
+      characterDirection: 'Right',
+    }
+
+    const [quote, setQuote] = React.useState(sampleQuote);
+    const getQuote = () => {
+    // Send the request
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        console.log(data);
+        setQuote(data[0]);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DisplayQuote quote={quote.quote} image={quote.image} character={quote.character}/>
+      <button type="button" onClick={getQuote}>Get Simpson quote</button>
     </div>
   );
 }
---
+++ src/Components/DisplayQuote.jsx
@@ -0,0 +1,13 @@
+import '../QuoteCard.css';
+
+const DisplayQuote = ({ quote, character, image }) => (
+    <figure className="QuoteCard">
+      <img src={image} alt={character} />
+      <figcaption>
+        <blockquote>{quote}</blockquote>
+        <cite>{character}</cite>
+      </figcaption>
+    </figure>
+  );
+  
+  export default DisplayQuote;
